50064006fb29c878ce22f7716fa26b75fe54fe53
Shiv Kumar <kumarshivsharma12@gmail.com>
2020-09-06 22:49:12 +0530

    Error Handeling...Hope it works

--- api/now-playing.ts
+++ api/now-playing.ts
@@ -5,34 +5,42 @@ import { Player } from '../components/NowPlaying';
 import { nowPlaying } from '../utils/spotify';
 
 export default async function (req: NowRequest, res: NowResponse) {
-  const { item = {} } = await nowPlaying();
+  try {
+    const { item = {} } = await nowPlaying();
 
-  const params = decode(req.url.split('?')[1]) as any;
+    const params = decode(req.url.split('?')[1]) as any;
 
-  if (params && typeof params.open !== 'undefined') {
-    if (item && item.external_urls) {
-      res.writeHead(302, {
-        Location: item.external_urls.spotify
-      });
-      return res.end();
+    if (params && typeof params.open !== 'undefined') {
+      if (item && item.external_urls) {
+        res.writeHead(302, {
+          Location: item.external_urls.spotify
+        });
+        return res.end();
+      }
+      return res.status(200).end();
     }
-    return res.status(200).end();
+
+    res.setHeader('Content-Type', 'image/svg+xml');
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
+
+    const { name: track } = item;
+    const { images = [] } = item.album || {};
+
+    const cover = images[images.length - 1]?.url;
+    let coverImg = null;
+    if (cover) {
+      const buff = await (await fetch(cover)).arrayBuffer();
+      coverImg = `data:image/jpeg;base64,${Buffer.from(buff).toString(
+        'base64'
+      )}`;
+    }
+
+    const artist = (item.artists || []).map(({ name }) => name).join(', ');
+    const text = renderToString(Player({ cover: coverImg, artist, track }));
+    return res.status(200).send(text);
+  } catch (error) {
+    console.log(error);
+    const text = renderToString(Player({ artist: null, track: null }));
+    return res.send(text);
   }
-
-  res.setHeader('Content-Type', 'image/svg+xml');
-  res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate');
-
-  const { name: track } = item;
-  const { images = [] } = item.album || {};
-
-  const cover = images[images.length - 1]?.url;
-  let coverImg = null;
-  if (cover) {
-    const buff = await (await fetch(cover)).arrayBuffer();
-    coverImg = `data:image/jpeg;base64,${Buffer.from(buff).toString('base64')}`;
-  }
-
-  const artist = (item.artists || []).map(({ name }) => name).join(', ');
-  const text = renderToString(Player({ cover: coverImg, artist, track }));
-  return res.status(200).send(text);
 }
